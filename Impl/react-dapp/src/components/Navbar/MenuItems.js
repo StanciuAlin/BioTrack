d07b598d14d040ff501bdd2ba5a1ce6e6c61cf7d
StanciuAlin <stanciualin26@gmail.com>
2021-07-26 21:59:41 +0300

    Add a navbar and footer template to the frontend part

---
+++ Impl/react-dapp/src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Register member',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Track product',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Reports',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+]
---
+++ Impl/react-dapp/src/global-styles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  html, body {
+    box-sizing: border-box;
+    margin: 0;
+    padding-top: 0;
+    font-family: 'Montserrat', sans-serif;
+    position: relative;
+    background: #fff;
+}`;
